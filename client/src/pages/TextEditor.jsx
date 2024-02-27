d7e8b740ea50c6f41ffc18c4034e44a07fe66da8
dbajollari1 <dbajollari1@gmail.com>
2024-02-26 19:58:30 -0500

    basis text editor page

---
+++ client/src/pages/TextEditor.jsx
@@ -0,0 +1,28 @@
+import Navbar from "../components/Navbar";
+import MyFooter from "../components/MyFooter";
+import TextEdit from "../components/TextEdit";
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Link, useParams} from 'react-router-dom';
+import NoPage from "./NoPage";
+
+
+export default function TextEditor() {
+
+    let {id} = useParams();
+
+
+    if(!id || Number(id) <= 0 || typeof(Number(id)) !== 'number' || !(Number.isInteger(Number(id)))) { 
+        return (<NoPage></NoPage>)
+    }
+  return (
+    <>
+        <Navbar />
+        <div className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto text-center mt-11">
+            <h1 className="font-bold text-3xl"> Your Cover Letter</h1>
+            <TextEdit id={id} ></TextEdit>
+        </div>
+        <MyFooter />
+    </>
+  );
+}
