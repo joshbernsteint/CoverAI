3d8c07b2799539400d2cf2d8e43ba211a5949775
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-03-03 15:19:35 -0500

    add form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,37 +7,42 @@ import {
 import SignUp from "./pages/SignUp";
 import SignUpClerk from "./pages/SignUpClerk";
 import { AuthProvider } from "./context/AuthContext";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EditProfile from "./pages/EditProfile";
 import NoPage from "./pages/NoPage";
-import CoverLetters from './pages/CoverLetters';
-import React, { useState } from 'react';
-import TextEditor from './pages/TextEditor';
-import CLContext from './CLContext';
+import CoverLetters from "./pages/CoverLetters";
+import React, { useState } from "react";
+import TextEditor from "./pages/TextEditor";
+import CLContext from "./CLContext";
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
 
 function App() {
-
-  const [activeCL, setActiveCL] = useState(JSON.parse(localStorage.getItem('activeCL') || `{"ops": [{"insert": "Place your cover letter here!"}]}`));
+  const [activeCL, setActiveCL] = useState(
+    JSON.parse(
+      localStorage.getItem("activeCL") ||
+        `{"ops": [{"insert": "Place your cover letter here!"}]}`
+    )
+  );
   const [t, setT] = useState(`${Date.now()}`);
   console.log(t);
 
   return (
     <>
       <AuthProvider>
-      <CLContext.Provider value={{activeCL, setActiveCL}}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/cover-letters" element={<CoverLetters />} />
-          <Route path="/text-editor/:id" element={<TextEditor />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
-      </CLContext.Provider>
-    </AuthProvider>
+        <CLContext.Provider value={{ activeCL, setActiveCL }}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/edit-profile" element={<EditProfile />} />
+              <Route path="/cover-letters" element={<CoverLetters />} />
+              <Route path="/text-editor/:id" element={<TextEditor />} />
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </BrowserRouter>
+        </CLContext.Provider>
+      </AuthProvider>
     </>
   );
 }
--- client/src/pages/EditProfile.jsx
+++ client/src/pages/EditProfile.jsx
@@ -2,12 +2,105 @@ import Navbar from "../components/Navbar";
 import MyFooter from "../components/MyFooter";
 
 import { FileInput, Label } from "flowbite-react";
+import { FloatingLabel } from "flowbite-react";
+import { Datepicker } from "flowbite-react";
+import { TextInput } from "flowbite-react";
+
+import React from "react";
+
+import Select from "react-select";
 
 export default function EditProfile() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    const due = e.target.due.value;
+    const file = e.target.file.files[0];
+
+    console.log(title, description, due, file);
+  };
+
+  const skillsOptions = [
+    { value: "python", label: "Python" },
+    { value: "java", label: "Java" },
+    { value: "c++", label: "C++" },
+    { value: "c#", label: "C#" },
+    { value: "javascript", label: "JavaScript" },
+    { value: "react", label: "React" },
+    { value: "vue", label: "Vue" },
+    { value: "angular", label: "Angular" },
+    { value: "node.js", label: "Node.js" },
+    { value: "express", label: "Express" },
+    { value: "mongodb", label: "MongoDB" },
+    { value: "sql", label: "SQL" },
+    { value: "postgresql", label: "PostgreSQL" },
+    { value: "graphql", label: "GraphQL" },
+    { value: "rest", label: "REST" },
+    { value: "django", label: "Django" },
+    { value: "flask", label: "Flask" },
+    { value: "html", label: "HTML" },
+    { value: "css", label: "CSS" },
+    { value: "sass", label: "SASS" },
+    { value: "bootstrap", label: "Bootstrap" },
+    { value: "tailwind", label: "Tailwind" },
+    { value: "material-ui", label: "Material-UI" },
+    { value: "chakra-ui", label: "Chakra-UI" },
+    { value: "ant-design", label: "Ant-Design" },
+    { value: "figma", label: "Figma" },
+    { value: "adobe-xd", label: "Adobe XD" },
+    { value: "sketch", label: "Sketch" },
+    { value: "invision", label: "InVision" },
+    { value: "zeplin", label: "Zeplin" },
+    { value: "git", label: "Git" },
+    { value: "github", label: "GitHub" },
+    { value: "gitlab", label: "GitLab" },
+    { value: "bitbucket", label: "Bitbucket" },
+    { value: "jenkins", label: "Jenkins" },
+    { value: "travis-ci", label: "Travis CI" },
+    { value: "docker", label: "Docker" },
+    { value: "kubernetes", label: "Kubernetes" },
+    { value: "aws", label: "AWS" },
+    { value: "gcp", label: "GCP" },
+    { value: "azure", label: "Azure" },
+    { value: "heroku", label: "Heroku" },
+    { value: "netlify", label: "Netlify" },
+    { value: "vercel", label: "Vercel" },
+    { value: "firebase", label: "Firebase" },
+    { value: "twilio", label: "Twilio" },
+    { value: "stripe", label: "Stripe" },
+    { value: "paypal", label: "PayPal" },
+    { value: "square", label: "Square" },
+    { value: "shopify", label: "Shopify" },
+    { value: "woocommerce", label: "WooCommerce" },
+    { value: "magento", label: "Magento" },
+    { value: "bigcommerce", label: "BigCommerce" },
+    { value: "salesforce", label: "Salesforce" },
+    { value: "sap", label: "SAP" },
+    { value: "oracle", label: "Oracle" },
+    { value: "mysql", label: "MySQL" },
+    { value: "mariadb", label: "MariaDB" },
+    { value: "sqlite", label: "SQLite" },
+    { value: "cassandra", label: "Cassandra" },
+    { value: "redis", label: "Redis" },
+    { value: "elasticsearch", label: "Elasticsearch" },
+    { value: "kibana", label: "Kibana" },
+    { value: "logstash", label: "Logstash" },
+    { value: "kafka", label: "Kafka" },
+    { value: "rabbitmq", label: "RabbitMQ" },
+    { value: "activemq", label: "ActiveMQ" },
+    { value: "zeromq", label: "ZeroMQ" },
+    { value: "websockets", label: "WebSockets" },
+    { value: "grpc", label: "gRPC" },
+    { value: "thrift", label: "Thrift" },
+    { value: "avro", label: "Avro" },
+    { value: "protocol-buffers", label: "Protocol Buffers" },
+  ];
+
   return (
     <>
       <Navbar />
-      <div className="h-screen mt-20 flex-row justify-center items-center container mx-auto">
+      <div className="mt-20 flex-row justify-center items-center container mx-auto">
         <div className="flex w-full items-center justify-center">
           <Label
             htmlFor="dropzone-file"
@@ -37,12 +130,63 @@ export default function EditProfile() {
                 PDF, DOCX, DOC, TXT
               </p>
             </div>
-            <FileInput id="dropzone-file" accept = ".pdf,.docs,.doc" className="hidden" />
+            <FileInput
+              id="dropzone-file"
+              accept=".pdf,.docs,.doc"
+              className="hidden"
+            />
           </Label>
         </div>
 
-        the form will be here
-        
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <FloatingLabel variant="standard" label="First Name" />
+
+          <FloatingLabel variant="standard" label="Last Name" />
+
+          <FloatingLabel variant="standard" label="Email" />
+
+          <FloatingLabel variant="standard" label="Phone Number" />
+
+          <FloatingLabel variant="standard" label="School Name" />
+
+          <FloatingLabel variant="standard" label="Major" />
+
+          <label>
+            Expected Graduation Date:
+            <Datepicker
+              minDate={new Date(2023, 0, 1)}
+              maxDate={new Date(2023, 3, 30)}
+            />
+          </label>
+
+          <label htmlFor="">
+            Select your skills:
+            <Select
+              options={skillsOptions}
+              isMulti={true}
+              placeholder="Select your skills"
+            />
+          </label>
+
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="large" value="Tell us more about you!" />
+            </div>
+            <TextInput id="large" type="text" sizing="lg" />
+          </div>
+
+          <input type="submit" value="Submit" />
+        </form>
       </div>
       <MyFooter />
     </>
