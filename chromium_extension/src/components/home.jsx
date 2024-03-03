8fe776054b13b943b12ea51412f7329db16a7a1e
Ajit Kandasamy <akandasa@stevens.edu>
2024-03-03 15:52:51 -0500

    fully integrated the scraper into the form

--- chromium_extension/src/components/home.jsx
+++ chromium_extension/src/components/home.jsx
@@ -1,14 +1,14 @@
-import { useRef } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import {Button, ButtonGroup, Dropdown} from 'react-bootstrap';
 import DocumentManager from '../documentManager';
-
+import DescriptionInput from './DescriptionInput.jsx'
 
 export default function Home({styleSheet, userSettings, ...props}){
 
     const settings = userSettings.current;
     const buttonRef = useRef(null);
     const formRef = useRef(null);
+    const [value, setValue] = useState("");
 
     async function handleOpenPreview(){
         console.log("Handling opening preview");
@@ -45,18 +45,19 @@ export default function Home({styleSheet, userSettings, ...props}){
     return (
     <div className='App-header'>
       <h1>CoverAI Menu</h1>
-        <Button onClick={toggleVisibility} ref={buttonRef}>Generate</Button>
-        <form ref={formRef} onSubmit={handleSubmit} hidden>
-                <label>
+        <Button onClick={toggleVisibility} ref={buttonRef}>Generate</Button> 
+            <form ref={formRef} onSubmit={handleSubmit} hidden>
+              <DescriptionInput toggleVisibility={toggleVisibility} value={value} setValue={setValue}/>
+                {/* <label>
                     <h2>Input your prompt below:</h2>
                     <textarea style={{width: "60%", height: "100px", fontFamily: "sans-serif"}} id="basic-input"/>
                 </label><br/>
                 <button onClick={() => toggleVisibility(false)}>
                     Cancel
-                </button>
-                <button type="submit">
-                    Submit
-                </button>
+                </button> */}
+              <button type="submit">
+                  Submit
+              </button>
             </form>
         <Dropdown as={ButtonGroup}>
       <Button onClick={handleOpenPreview}>Open Preview</Button>
