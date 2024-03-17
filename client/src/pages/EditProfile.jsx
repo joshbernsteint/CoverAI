62494b89f9b706af9345288dbd37cd73e8abee00
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-03-17 14:38:30 -0400

    display object being saved and add temp api call

--- client/src/pages/EditProfile.jsx
+++ client/src/pages/EditProfile.jsx
@@ -12,77 +12,62 @@ import Select from "react-select";
 
 import { useState } from "react";
 
+import { motion as m } from "framer-motion";
+
 export default function EditProfile() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [schoolName, setSchoolName] = useState("");
-  const [major, setMajor] = useState("");
-  const [graduationDate, setGraduationDate] = useState("");
-  const [skills, setSkills] = useState([]);
-  const [description, setDescription] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //set grad date and skills
-    const gradDate = document.getElementById("gradDate").value;
-    console.log("grad date:", gradDate);
-    setGraduationDate(gradDate);
-    const selectedSkills = document.getElementById("skills").value;
-    console.log("selected skills:", selectedSkills);
-    setSkills(selectedSkills);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    schoolName: "",
+    major: "",
+    graduationDate: "",
+    skills: "",
+    description: "",
+  });
 
-    if (firstName === "") {
-      alert("First name is required");
-      return;
-    }
-    if (lastName === "") {
-      alert("Last name is required");
-      return;
-    }
-    if (email === "") {
-      alert("Email is required");
-      return;
-    }
-    if (phoneNumber === "") {
-      alert("Phone number is required");
-      return;
-    }
-    if (schoolName === "") {
-      alert("School name is required");
-      return;
-    }
-    if (major === "") {
-      alert("Major is required");
-      return;
-    }
-    if (gradDate === "") {
-      alert("Graduation date is required");
-      return;
-    }
-    if (skills.length === 0) {
-      alert("Skills are required");
-      return;
-    }
-    if (description === "") {
-      alert("Description is required");
-      return;
-    }
-
-    console.log(
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-      schoolName,
-      major,
-      graduationDate,
-      skills,
-      description
-    );
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
+  const handleSubmit = async (e) => {
+    console.log("Form submitted:", formData);
+    e.preventDefault();
+
+    //REAL API CALL GOES HERE THIS IS JUST A MOCK
+    try {
+      const response = await fetch("/createProfile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log("Profile created successfully");
+        // You can perform additional actions here, such as redirecting the user or showing a success message
+      } else {
+        console.error("Failed to create profile");
+        // Handle error appropriately
+      }
+    } catch (error) {
+      console.error("Error creating profile:", error);
+      // Handle error appropriately
+    }
+  };
+
+  const handleButtonClick = () => {
+    setShowForm(true);
+  };
+
+  //will be used for dropdown menu later
   const skillsOptions = [
     { value: "python", label: "Python" },
     { value: "java", label: "Java" },
@@ -159,13 +144,106 @@ export default function EditProfile() {
     { value: "protocol-buffers", label: "Protocol Buffers" },
   ];
 
-  const today = new Date();
-
   return (
     <>
       <Navbar />
+      {showForm && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-75 z-50">
+          <div className="bg-white p-8 rounded-lg">
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              placeholder="First Name"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              placeholder="Last Name"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="tel"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              placeholder="Phone Number"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="schoolName"
+              value={formData.schoolName}
+              onChange={handleChange}
+              placeholder="School Name"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="major"
+              value={formData.major}
+              onChange={handleChange}
+              placeholder="Major"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="date"
+              name="graduationDate"
+              value={formData.graduationDate}
+              onChange={handleChange}
+              placeholder="Graduation Date"
+              className="px-4 py-2 border rounded-md"
+            />
+            <input
+              type="text"
+              name="skills"
+              value={formData.skills}
+              onChange={handleChange}
+              placeholder="Skills (comma-separated)"
+              className="px-4 py-2 border rounded-md"
+            />
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Description"
+              className="px-4 py-2 border rounded-md"
+              rows="4"
+            ></textarea>
+
+            <div className="flex gap-2 mt-4">
+              <button
+                onClick={() => setShowForm(false)}
+                className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
+              >
+                Close
+              </button>
+
+              <button
+                onClick={handleSubmit}
+                type="submit"
+                className="px-7 py-2 bg-coverLetterBlue text-white rounded hover:bg-secondary"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="flex justify-center">
-        <div className="border-4 p-12 w-full">
+        <div className="p-12 w-full">
           <div className="flex justify-center text-3xl font-semibold mb-4">
             Get started by filling out your profile
           </div>
@@ -221,7 +299,10 @@ export default function EditProfile() {
               <div className="flex flex-col items-center text-center gap-2">
                 <div className="text-2xl">Fill out this form</div>
                 <div>
-                  <button className="px-7 py-2 bg-coverLetterBlue text-white rounded hover:bg-secondary">
+                  <button
+                    onClick={handleButtonClick}
+                    className="px-7 py-2 bg-coverLetterBlue text-white rounded hover:bg-secondary"
+                  >
                     Take me there
                   </button>
                 </div>
@@ -230,7 +311,6 @@ export default function EditProfile() {
           </div>
         </div>
       </div>
-
       <MyFooter />
     </>
   );
