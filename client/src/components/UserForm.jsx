be50dbd5ec719c752cec35e292f7a26b00af3267
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-03-17 17:25:00 -0400

    fix form

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 
-
 export default function UserForm() {
   const [showForm, setShowForm] = useState(false);
 
@@ -54,8 +53,6 @@ export default function UserForm() {
       console.error("Error creating profile:", error);
       // Handle error appropriately
     }
-
-    
   };
 
   return (
@@ -63,78 +60,106 @@ export default function UserForm() {
       {showForm && (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-75 z-50">
           <div className="bg-white p-8 rounded-lg">
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              placeholder="First Name"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              placeholder="Last Name"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Email"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="tel"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              placeholder="Phone Number"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="text"
-              name="schoolName"
-              value={formData.schoolName}
-              onChange={handleChange}
-              placeholder="School Name"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="text"
-              name="major"
-              value={formData.major}
-              onChange={handleChange}
-              placeholder="Major"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="date"
-              name="graduationDate"
-              value={formData.graduationDate}
-              onChange={handleChange}
-              placeholder="Graduation Date"
-              className="px-4 py-2 border rounded-md"
-            />
-            <input
-              type="text"
-              name="skills"
-              value={formData.skills}
-              onChange={handleChange}
-              placeholder="Skills (comma-separated)"
-              className="px-4 py-2 border rounded-md"
-            />
-            <textarea
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-              placeholder="Description"
-              className="px-4 py-2 border rounded-md"
-              rows="4"
-            ></textarea>
+            <div className="mb-4 flex gap-2">
+              <div>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="First Name"
+                  className="px-4 py-2 border rounded-md"
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Last Name"
+                  className="px-4 py-2 border rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Email"
+                className="px-4 py-2 border rounded-md"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="tel"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                placeholder="Phone Number"
+                className="px-4 py-2 border rounded-md"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="text"
+                name="schoolName"
+                value={formData.schoolName}
+                onChange={handleChange}
+                placeholder="School Name"
+                className="px-4 py-2 border rounded-md"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="text"
+                name="major"
+                value={formData.major}
+                onChange={handleChange}
+                placeholder="Major"
+                className="px-4 py-2 border rounded-md"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="date"
+                name="graduationDate"
+                value={formData.graduationDate}
+                onChange={handleChange}
+                placeholder="Graduation Date"
+                className="px-4 py-2 border rounded-md"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="text"
+                name="skills"
+                value={formData.skills}
+                onChange={handleChange}
+                placeholder="Skills (comma-separated)"
+                className="px-4 py-2 border rounded-md"
+              />
+            </div>
+
+            <div>
+              <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                placeholder="Description"
+                className="px-4 py-2 border rounded-md"
+                rows="4"
+              ></textarea>
+            </div>
 
             <div className="flex gap-2 mt-4">
               <button
