d7bb2f75a28bba7f6936459524cb8b2a0bc4fc7e
semphoenix <rodriguezs2021@gmail.com>
2024-03-19 00:05:07 -0400

    Hard coded resumeData

--- server/src/api/covers/covers.controller.js
+++ server/src/api/covers/covers.controller.js
@@ -119,6 +119,73 @@ router.route("/genCoverLetter").post(async (req, res, next) => {
   try {
     // const user_id = req.auth.sessionClaims.sub;
     const user_id = "user_2dC6mNNpMcxT5kubchWOsfUs2TB";
+    resumeData = {
+      _id: {
+        $oid: "65f8ebc97b9b5caa9e6cce25",
+      },
+      userId: "65f74a1ef1a8f10d860bb03f",
+      resumeType: "pdf",
+      extractedText:
+        "Joshua Bernstein  Mahwah, NJ, 07430   |   201-249-3266 | joshuatbernstein@gmail.com | joshbernsteint.github.io | LinkedIn  EDUCATION & LANGUAGES  Stevens Institute of Technology,   Hoboken, NJ—   Bachelors of Computer Science   (2021 – Spring 2024)  - 3.9 GPA, Stevens Pinnacle Scholar. Expected graduation date Spring 2024.  - Relevant coursework: Data structures–A, Systems Programming–A, Algorithms–A, Computer Architecture–A  Stevens Institute of Technology,   Hoboken, NJ—   Masters of Computer Science   (2024 – Spring 2025)  -Beginning Graduate Program Fall 2024. Graduating with my Bachelors and Masters of Computer Science in 4 years.  Languages  - Fluent English skills, including speaking, writing, and reading.  - Professional French Skills, including speaking, writing, and reading (Recipient of the NJDOE Seal of Biliteracy in French)  SKILLS  - Experience and expertise in a variety of di \u0000 erent programming languages including:  - Python, C/C++, Java, ARMv8 Assembly, Javascript, Racket, OCaml, R, C#, Bash  - Seasoned with Git and utilizing continuous integration tools such as GitHub Actions.  - Versed in utilizing cloud-based virtual machines via Secure Socket Shell(SSH) Protocol.  - Pro fi cient with database management systems and engines such as PostgresQL and MongoDB.  EXPERIENCE  Student Assistant—   Stevens Institute of Technology, Hoboken, NJ   (Oct 2023 – Present)  - Designed and programmed an interactive dashboard capable of viewing, editing, and analyzing speci fi c model data.  - Coordinated with 6 other team members to integrate their contributions into the dashboard e \u0000 ciently.  - Delivered periodic presentations and demonstrations to over 40 supervisors and project clients.  Course Assistant—   Stevens Institute of Technology, Hoboken, NJ   (Sept 2023 – Present)  - Re fi ned class assignments and homeworks to ensure a lack of typographical and logical errors.  - Taught weekly lab sessions to 35 students to cement their understanding in the curriculum.  - Streamlined the grading process by constructing a script to automatically grade homeworks for 200 students.  - Conducted weekly o \u0000 ce hours to answer any questions regarding the subject material.  Research Assistant –   Stevens Institute of Technology, Hoboken, NJ   (Summer 2022, Summer 2023)  - Spearheaded design and construction of Python scripts to download, analyze, and store over 690,000 image   fi les safely.  - Utilized aforementioned scripts to e \u0000 ectively   fi lter images to curate a sample of 3,600 high-quality image   fi les.  - Implemented scripts to interact with GIS software technologies such as ArcGIS to correlate geographic image metadata  with a 2D map of Mercury to visualize physical locations of each image   fi le.  - Coordinated with 4 colleagues in the research group to facilitate e \u0000 cient and standardized code.  PROJECTS  ANGEL   —   Node-based Downloader and Converter Application   (Summer 2023) – GitHub Repository  - Electron-based Node and React application to download and convert audio and video   fi les.  - Features ability to choose download quality, either through video resolution, audio quality, or frame rate.  - Utilizes   \u0000 mpeg for video and audio conversion from any supported container format to another.  - Editable settings including di \u0000 erent view modes, custom download preferences, and accessibility options.  Scrumptious Solar Services —   Full-stack Web Application   (Spring 2023) – GitHub Repository  - Directed a 6-person team in the production of a web application to streamline the processes of a solar panel company.  - Integrated several di \u0000 erent technologies, including React, Express, Stripe, and Firebase in the end-product.  - Implemented Agile development methodology over 4 two-week sprints, producing a workable demo each sprint.  - Recipient of “Outstanding Project Award” out of 35 total teams by the project client.  VOLUNTEER & EXTRACURRICULARS  Vice President —   Stevens Computer Science Club   (Fall 2023 – Present)  - Composed weekly newsletter to inform the over 300 club members of important events and opportunities.  - Ensured club meetings had a room reserved with adequate space for all members.  - Guided workshops and presentations on essential computer science topics.  AWARDS & ACCOLADES  - Stevens Institute of Technology Dean’s List– Fall 2021, Spring 2022, Fall 2022, Spring 2023  - Outstanding Project Award in recognition of work done on   Scrumptious Solar Services   – Spring 2023",
+      extractedSections: [
+        {
+          name: "Education",
+          content: "EDUCATION & LANGUAGES",
+          extracted:
+            "Stevens Institute of Technology, \nHoboken, NJ— \nBachelors of Computer Science \n(2021 – Spring 2024)\n- 3.9 GPA, Stevens Pinnacle Scholar. Expected graduation date Spring 2024.\n- Relevant coursework: Data structures–A, Systems Programming–A, Algorithms–A, Computer Architecture–A\nStevens Institute of Technology, \nHoboken, NJ— \nMasters of Computer Science \n(2024 – Spring 2025)\n-Beginning Graduate Program Fall 2024. Graduating with my Bachelors and Masters of Computer Science in 4 years.\nLanguages\n- Fluent English skills, including speaking, writing, and reading.\n- Professional French Skills, including speaking, writing, and reading (Recipient of the NJDOE Seal of Biliteracy in French)",
+        },
+        {
+          name: "Skills or Extracurriculars",
+          content: "SKILLS",
+          extracted:
+            "- Experience and expertise in a variety of di  erent programming languages including:\n- Python, C/C++, Java, ARMv8 Assembly, Javascript, Racket, OCaml, R, C#, Bash\n- Seasoned with Git and utilizing continuous integration tools such as GitHub Actions.\n- Versed in utilizing cloud-based virtual machines via Secure Socket Shell(SSH) Protocol.\n- Pro fi cient with database management systems and engines such as PostgresQL and MongoDB.",
+        },
+        {
+          name: "Experience",
+          content: "EXPERIENCE",
+          extracted:
+            "Student Assistant— \nStevens Institute of Technology, Hoboken, NJ \n(Oct 2023 – Present)\n- Designed and programmed an interactive dashboard capable of viewing, editing, and analyzing speci fi c model data.\n- Coordinated with 6 other team members to integrate their contributions into the dashboard e  ciently.\n- Delivered periodic presentations and demonstrations to over 40 supervisors and project clients.\nCourse Assistant— \nStevens Institute of Technology, Hoboken, NJ \n(Sept 2023 – Present)\n- Re fi ned class assignments and homeworks to ensure a lack of typographical and logical errors.\n- Taught weekly lab sessions to 35 students to cement their understanding in the curriculum.\n- Streamlined the grading process by constructing a script to automatically grade homeworks for 200 students.\n- Conducted weekly o  ce hours to answer any questions regarding the subject material.\nResearch Assistant – \nStevens Institute of Technology, Hoboken, NJ \n(Summer 2022, Summer 2023)\n- Spearheaded design and construction of Python scripts to download, analyze, and store over 690,000 image \nfi les safely.\n- Utilized aforementioned scripts to e  ectively \nfi lter images to curate a sample of 3,600 high-quality image \nfi les.\n- Implemented scripts to interact with GIS software technologies such as ArcGIS to correlate geographic image metadata\nwith a 2D map of Mercury to visualize physical locations of each image \nfi le.\n- Coordinated with 4 colleagues in the research group to facilitate e  cient and standardized code.",
+        },
+        {
+          name: "Projects",
+          content: "PROJECTS",
+          extracted:
+            "ANGEL \n— \nNode-based Downloader and Converter Application \n(Summer 2023) – GitHub Repository\n- Electron-based Node and React application to download and convert audio and video \nfi les.\n- Features ability to choose download quality, either through video resolution, audio quality, or frame rate.\n- Utilizes \n mpeg for video and audio conversion from any supported container format to another.\n- Editable settings including di  erent view modes, custom download preferences, and accessibility options.\nScrumptious Solar Services — \nFull-stack Web Application \n(Spring 2023) – GitHub Repository\n- Directed a 6-person team in the production of a web application to streamline the processes of a solar panel company.\n- Integrated several di  erent technologies, including React, Express, Stripe, and Firebase in the end-product.\n- Implemented Agile development methodology over 4 two-week sprints, producing a workable demo each sprint.\n- Recipient of “Outstanding Project Award” out of 35 total teams by the project client.",
+        },
+        {
+          name: "Skills or Extracurriculars",
+          content: "VOLUNTEER & EXTRACURRICULARS",
+          extracted:
+            "Vice President — \nStevens Computer Science Club \n(Fall 2023 – Present)\n- Composed weekly newsletter to inform the over 300 club members of important events and opportunities.\n- Ensured club meetings had a room reserved with adequate space for all members.\n- Guided workshops and presentations on essential computer science topics.",
+        },
+        {
+          name: "Awards or Honors",
+          content: "AWARDS & ACCOLADES",
+          extracted:
+            "- Stevens Institute of Technology Dean’s List– Fall 2021, Spring 2022, Fall 2022, Spring 2023\n- Outstanding Project Award in recognition of work done on \nScrumptious Solar Services \n– Spring 2023",
+        },
+      ],
+      pdfJSON: {
+        name: "Joshua Bernstein",
+        email: "joshuatbernstein@gmail.com",
+        phone: "201-249-3266",
+        education: {
+          school: "Stevens Institute of Technology,",
+          location: "Hoboken, NJ—",
+          degree: "",
+          fieldOfStudy: "",
+          to: {
+            month: "graduation",
+            year: "date",
+          },
+          gpa: "",
+          scale: "",
+          awards: "",
+          courses: "",
+        },
+        experience: {},
+      },
+    };
     const {
       employer_name,
       job_title,
