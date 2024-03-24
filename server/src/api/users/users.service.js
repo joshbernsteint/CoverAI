3b8d6ffb9caff8f5ab471de3eb9fad6111b1807c
semphoenix <rodriguezs2021@gmail.com>
2024-03-24 01:15:56 -0400

    Added functions for user skills and settings, also changed the structure of cover letter data and normalized some fields

--- server/src/api/users/users.service.js
+++ server/src/api/users/users.service.js
@@ -1,23 +1,63 @@
-import { z } from "zod";
 import { users } from "../../config/mongoCollections.js";
 import { UnexpectedError } from "../../utils/errors.js";
 import { clerkClient } from "@clerk/clerk-sdk-node";
 
-const userSchema = z.object({
-  uuid: z.string(),
-  firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, "Last name is required"),
-});
+const getSkills = async (user_id) => {
+  const userCollection = await users();
+  const user = await userCollection.findOne({ _id: user_id });
+  return user.skills;
+};
 
-const addSkills = async (user_id, skills) => {
+const setSkills = async (user_id, skills) => {
   const userCollection = await users();
   const updateResult = await userCollection.updateOne(
     { _id: user_id },
     { $set: { skills } }
   );
   if (updateResult.modifiedCount === 0)
-    throw new UnexpectedError("Failed to add skills to user.");
-  return "Skills added successfully.";
+    throw new UnexpectedError("Failed to edit user skills.");
+  const updatedUser = await userCollection.findOne({ _id: user_id });
+  return updatedUser;
 };
 
-export { addSkills };
+const getSettings = async (user_id) => {
+  const userCollection = await users();
+  const user = await userCollection.findOne({ _id: user_id });
+  return user.settings;
+};
+
+const setSettings = async (user_id, settings) => {
+  const userCollection = await users();
+  const updateResult = await userCollection.updateOne(
+    { _id: user_id },
+    { $set: { settings } }
+  );
+  if (updateResult.modifiedCount === 0)
+    throw new UnexpectedError("Failed to edit user settings.");
+  const updatedUser = await userCollection.findOne({ _id: user_id });
+  return updatedUser;
+};
+
+const resetSettings = async (user_id) => {
+  const userCollection = await users();
+  const updateResult = await userCollection.updateOne(
+    { _id: user_id },
+    {
+      $set: {
+        settings: {
+          dark_mode: false,
+          suggest_cl: false,
+          auto_download_cl: false,
+          save_resumes: false,
+          save_cl: false,
+        },
+      },
+    }
+  );
+  if (updateResult.modifiedCount === 0)
+    throw new UnexpectedError("Failed to reset user settings.");
+  const updatedUser = await userCollection.findOne({ _id: user_id });
+  return updatedUser;
+};
+
+export { setSkills, getSkills, setSettings, getSettings, resetSettings };
