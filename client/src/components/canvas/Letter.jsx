d264d75afc3b8f8143be70c1a941a29dfbd6515d
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-04-01 22:30:44 -0400

    protected routes and api calls

---
+++ client/src/components/canvas/Letter.jsx
@@ -0,0 +1,24 @@
+import React, { Suspense } from "react";
+
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
+
+const Letter = () => {
+  return (
+    <div className="z-0">
+      <Spline
+        scene="https://prod.spline.design/n64JWnDcuDS0-WQs/scene.splinecode"
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: "-20",
+          left: "",
+          zIndex: "2",
+          marginBottom: "10",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Letter;
