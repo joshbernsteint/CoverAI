e1f1cec66b97edb74fe9c2f075f991ffa8582ffb
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-04-01 23:00:01 -0400

    click cover letter to editor

--- client/src/components/canvas/Letter.jsx
+++ client/src/components/canvas/Letter.jsx
@@ -5,18 +5,20 @@ const Spline = React.lazy(() => import("@splinetool/react-spline"));
 const Letter = () => {
   return (
     <div className="z-0">
-      <Spline
-        scene="https://prod.spline.design/n64JWnDcuDS0-WQs/scene.splinecode"
-        style={{
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          top: "-20",
-          left: "",
-          zIndex: "2",
-          marginBottom: "10",
-        }}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline
+          scene="https://prod.spline.design/n64JWnDcuDS0-WQs/scene.splinecode"
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: "-20",
+            left: "",
+            zIndex: "2",
+            marginBottom: "10",
+          }}
+        />
+      </Suspense>
     </div>
   );
 };
