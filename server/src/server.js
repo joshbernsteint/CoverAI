4633122061367c6da9d40d184e47f96e3298eea1
Joshua Bernstein <joshuatbernstein@gmail.com>
2024-04-13 16:12:02 -0400

    changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,22 @@
 import app from "./app.js";
+import fs from 'fs';
+import https from 'https';
 
-app.listen(80, function () {
-  console.log(`🚀 Server running on ${process.env.SERVER_URL}`);
-});
+
+const inDevelopmentMode = process.env.IN_DEV === "true" || false;
+
+if(inDevelopmentMode){
+  app.listen(process.env.PORT , function () {
+    console.log(`🚀 Server running on ${process.env.SERVER_URL}`);
+  });
+}
+else{
+  const httpsOptions = inDevelopmentMode ? {} : {
+    pfx: fs.readFileSync(process.env.CERT_PATH),
+    passphrase: "",
+  };
+  const httpsServer =  https.createServer(httpsOptions, app);
+  httpsServer.listen((process.env.PORT || 3000), () => {
+    console.log(`🚀 Server running on ${process.env.SERVER_URL}`);
+  });
+}
