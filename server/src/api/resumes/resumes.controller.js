1936e16377ccd394cb2ada603df5482f5882a78f
Joshua Bernstein <joshuatbernstein@gmail.com>
2024-04-13 16:37:48 -0400

    changes

--- server/src/api/resumes/resumes.controller.js
+++ server/src/api/resumes/resumes.controller.js
@@ -55,7 +55,7 @@ router.post(
       );
       return res.status(200).json(data);
     } catch (error) {
-      console.log(error);
+      console.log("ERROR:", error.toString());
       return res.status(error.status || 500).json({ message: error.message });
     }
   }
